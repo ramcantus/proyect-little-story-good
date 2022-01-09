64e51fbcf2c1ad6b36dc122fa57687d5b64e2e9c
ramcantus <97369502+ramcantus@users.noreply.github.com>
2022-01-08 17:26:26 -0800

    Add files via upload

---
+++ proyect little story good/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Apiole.Title = "My Little story";
+Console.ForegroundColor = ConsoleColor.Green;
+ // Get Conversation going
+Console.WriteLine("Hey, What would you like to order?");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.ReadLine();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("You sure you want that?");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.ReadLine();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("You know you are a pussy for ordering that?");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.ReadLine();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("What did you just say to me? \n You're dead!");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.ReadLine();
+ // Death Scene
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("*Dies from to much sex :v*");
+Console.ReadKey();
+ // Finish
